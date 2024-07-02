87782bd93d53e2f83636b7ddeef78686e9d7c3b7
Luis Frank Florentino Medrano <lflorentinomedrano@gmail.com>
2024-07-02 01:49:07 -0500

    add privacy policies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PrivacyPoliciesComponent } from './privacy-policies/privacy-policies.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: 'Privacy-Policies', component: PrivacyPoliciesComponent },
+{ path: '', component: HomeComponent },
+];
