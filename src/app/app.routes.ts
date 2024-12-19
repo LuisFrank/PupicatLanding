d0f245cd7f33435d42000cc93203522c1fd58024
Luis Frank Florentino Medrano <lflorentinomedrano@gmail.com>
2024-12-19 01:53:41 -0500

    se agrega footer y faqs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { PrivacyPoliciesComponent } from './privacy-policies/privacy-policies.component';
 import { HomeComponent } from './home/home.component';
+import { FaqsComponent } from './faqs/faqs.component'; // Importa el componente de FAQs
+import { FooterComponent } from './footer/footer.component'; // Importa el componente de Footer
 
 export const routes: Routes = [
-{ path: 'Privacy-Policies', component: PrivacyPoliciesComponent },
-{ path: '', component: HomeComponent },
+  { path: 'Privacy-Policies', component: PrivacyPoliciesComponent },
+  { path: 'Faqs', component: FaqsComponent }, // Agrega la ruta para FAQs
+  { path: 'Footer', component: FooterComponent }, // Agrega la ruta para Footer
+  { path: '', component: HomeComponent },
 ];
