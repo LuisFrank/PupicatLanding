0f3ee2f36c469a6fbfbd68cac5f1b7606c3cbe6c
Luis Frank Florentino Medrano <lflorentinomedrano@gmail.com>
2025-06-30 01:01:31 -0500

    add component terms conditions"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './home/home.component';
 import { FaqsComponent } from './faqs/faqs.component'; // Importa el componente de FAQs
 import { FooterComponent } from './footer/footer.component'; // Importa el componente de Footer
 import { ComplaintsBookComponent } from './complaints-book/complaints-book.component';
+import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
 export const routes: Routes = [
   { path: 'Privacy-Policies', component: PrivacyPoliciesComponent },
   { path: 'Faqs', component: FaqsComponent }, // Agrega la ruta para FAQs
   { path: 'Footer', component: FooterComponent }, // Agrega la ruta para Footer
   { path: 'Complaints-Book', component: ComplaintsBookComponent },
+  { path: 'terms-conditions', component: TermsConditionsComponent },
   { path: '', component: HomeComponent },
 ];
---
+++ src/app/complaints-book/complaints-book.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-complaints-book',
+  standalone: true,
+  imports: [],
+  templateUrl: './complaints-book.component.html',
+  styleUrl: './complaints-book.component.css'
+})
+export class ComplaintsBookComponent {
+
+}
